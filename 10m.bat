7002f8798f921e813115e78f412611683a60af8f
MonkeyyNinja <65881097+MonkeyyNinja@users.noreply.github.com>
2022-01-05 18:20:10 -0600

    Update 10m.bat

--- 10m.bat
+++ 10m.bat
@@ -1,2 +1,2 @@
 cd /D "%~dp0"
-rcon -a 127.0.0.1:27015 -p Pass2worDD "servermsg \"Server will restart in 10 minutes\""
+rcon -a 127.0.0.1:27015 -p password "servermsg \"Server will restart in 10 minutes\""
