e257c36c0e64d244ed29a200ea25409d3bf89e51
MonkeyyNinja <65881097+MonkeyyNinja@users.noreply.github.com>
2022-01-05 18:19:50 -0600

    Update 1m.bat

--- 1m.bat
+++ 1m.bat
@@ -1,2 +1,2 @@
 cd /D "%~dp0"
-rcon -a 127.0.0.1:27015 -p Pass2worDD "servermsg \"Server will restart in 1 minute'\"
+rcon -a 127.0.0.1:27015 -p password "servermsg \"Server will restart in 1 minute'\"
